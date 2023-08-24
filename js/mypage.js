7d5b37bdd435c497d54cd3fb4972a8cf763bfd1b
flowerkorr <d7983029613@daum.net>
2023-08-24 20:44:41 +0900

    우동헬/비번찾기/회원정보 수정  페이지연결 작업 완료 회원정보 수정 js  경로작업 완료

--- js/mypage.js
+++ js/mypage.js
@@ -2,7 +2,7 @@ function call_confirm(){
 
 	if(confirm("정.말.로 탈퇴하시겠습니까?")){
         alert("탈퇴되었습니다 :(");
-        window.location.href= "../main/main.html";
+        window.location.href= "/main/pro/main.html";
 	}else{
 		
 	}
